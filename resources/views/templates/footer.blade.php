290174d127f392310426f00c21326c150f2caf07
Riccarpa <riccardopagni99@gmail.com>
2021-10-01 14:37:31 +0200

    header+footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,47 @@
+<section  >
+    <div class="col">
+        <ul>
+            <h3>DC COMICS</h3>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+        </ul>
+        <ul>
+            <h3>SHOP</h3>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+        </ul>
+    </div>
+    <div class="col">
+        <ul>
+            <h3>DC</h3>
+            <li>Terms of use</li>
+            <li>Privacy and policy</li>
+            <li>Ad choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscription</li>
+            <li>Talent Workshop</li>
+            <li>CPSC Certification</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact us</li>
+        </ul>
+    </div>
+    <div class="col">
+        <ul>
+            <h3>SITES</h3>
+            <li>DC</li>
+            <li>MAD magazine</li>
+            <li>DC Kids</li>
+            <li>DC universe</li>
+            <li>DC Power Visa</li>
+        </ul>
+    </div>
+
+  </section>
+<img src="images/dc-logo.png" alt="">
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div class="top-header"></div>
+    <div class="container">
+        <figure>
+            <img class="pointer" src="images/dc-logo.png" alt="">
+        </figure>
+        <ul>
+            <li class="pointer">CHARACTERS</li>
+            <li class="pointer">COMICS</li>
+            <li class="pointer">MOVIES</li>
+            <li class="pointer">TV</li>
+            <li class="pointer">GAMES</li>
+            <li class="pointer">COLLECTIBLES</li>
+            <li class="pointer">VIDEOS</li>
+            <li class="pointer">FANS</li>
+            <li class="pointer">NEWS</li>
+            <li class="pointer">SHOP</li>
+
+        </ul>
+        <input type="search" placeholder="search &#x1F50D;">
+    </div>
+</body>
+</html>
